c3eee8e15fafc4c455ff1b461bc7906fd4efc842
Abhinandan Yadav <abhinandanyv777@gmail.com>
2024-08-27 16:16:28 +0530

    commit4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,94 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	let arr = [
+		[1, 0, 1],
+		[0, 1, 0],
+		[1, 0, 1]
+	];
+	let position = [];
+	const handleClick = (e, pos) => {
+		// position = [...position, pos];
+		const { i, j } = pos;
+		position.unshift({ op: 1 - arr[i][j], pos });
+		position = position;
+
+		arr[i][j] = 1 - arr[i][j];
+		const status = arr[i][j];
+		if (status) {
+			e.target.classList.add('dark');
+			e.target.classList.remove('light');
+		} else {
+			e.target.classList.remove('dark');
+			e.target.classList.add('light');
+		}
+
+		console.log(position);
+	};
+	const handleReplay = () => {
+		if (position.length > 0) {
+			const element = position.pop();
+			// console.log(element);
+			const { i, j } = element.pos;
+			arr[i][j] = 1 - element.op;
+		}
+		console.log(position);
+	};
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<div>
+		{#each arr as row, i}
+			<div class="row">
+				{#each row as ele, j}
+					<div
+						class={ele === 1 ? 'ele dark' : 'ele light'}
+						on:click={(e) => {
+							handleClick(e, { i, j });
+							console.log('clicked');
+						}}
+					></div>
+				{/each}
+			</div>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<Counter />
+			<br />
+		{/each}
+	</div>
+	<center><button on:click={handleReplay}>replay</button></center>
 </section>
 
 <style>
+	button {
+		width: 320px;
+		height: 50px;
+		border-radius: 10px;
+	}
+
 	section {
 		display: flex;
-		/* flex-direction: column; */
 		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+		flex-direction: column;
+		text-align: center;
+		/* width: 500px;
+		height: 500px; */
+		/* background-color: blue; */
+	}
+	.row {
+		display: flex;
+		justify-content: center;
 	}
 
-	h1 {
-		width: 100%;
+	.ele {
+		width: 100px;
+		height: 100px;
+		display: flex;
+		justify-content: center;
+		text-align: center;
+		/* background-color: blue; */
+		margin: 10px;
 	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	.dark {
+		background-color: rgb(121, 115, 115);
 	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.light {
+		background-color: white;
 	}
 </style>
